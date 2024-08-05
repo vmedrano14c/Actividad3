f31ca5084bd85fc4b54c67f0babc13456561aedb
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2024-08-04 23:00:45 -0500

    Ejercicio18

---
+++ ejercicioscp3/ejercicio18/Ejercicio18.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicioscp3.ejercicio18;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio18 {
+
+    public static void main(String[] args) {
+        Calculador form = new Calculador();
+        form.setVisible(true);
+    }
+}
