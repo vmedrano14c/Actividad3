1a1d0ff5ef0b4d2b8846e1658302d8eb10371b60
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2024-08-05 19:05:58 -0500

    Ejercicios

---
+++ ejercicioscp3/ejercicio19/Ejercicio19.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicioscp3.ejercicio19;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio19 {
+
+    public static void main(String[] args) {
+        Triangulo form = new Triangulo();
+        form.setVisible(true);
+    }
+}
---
+++ ejercicioscp4/ejercicio7/Ejercicio7.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicioscp4.ejercicio7;
+
+/**
+ *
+ * @author USUARIO
+ */
+
+public class Ejercicio7 {
+	
+	public static void main(String[] args) {
+        Analisis form = new Analisis();
+        form.setVisible(true);
+    }
+}
